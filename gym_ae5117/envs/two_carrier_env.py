correct twocarrier rotation and reward

gym_ae5117/envs/two_carrier_env.py
@@ -14,7 +14,7 @@ class TwoCarrierEnv(gym.Env):
         self.seed()
         self.viewer = None
         self.prev_reward = None
-        self.max_episode_steps = 1000
+        self.max_episode_steps = 500
         self.observation_space = spaces.Box(low=-10., high=10., shape=(3,), dtype=np.float32)
         self.action_space = spaces.Tuple((spaces.Discrete(4), spaces.Discrete(4))) # ^v<>
         self.action_codebook = np.array([
@@ -43,7 +43,7 @@ class TwoCarrierEnv(gym.Env):
         wwpat = Rectangle(xy=(-5.5,-.5), width=.5, height=6, fc='gray')
         ewpat = Rectangle(xy=(5,-.5), width=.5, height=6, fc='gray')
         swpat = Rectangle(xy=(-5.5,-.5), width=11, height=.5, fc='gray')
-        self.wall_patches = [nwwpat, newpat, wwpat, ewpat, swpat]
+        self.fixed_patches = [nwwpat, newpat, wwpat, ewpat, swpat]
             
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -64,8 +64,8 @@ class TwoCarrierEnv(gym.Env):
             self.rod_pose[0]-.5*np.cos(self.rod_pose[-1]), 
             self.rod_pose[1]-.5*np.sin(self.rod_pose[-1])
         ])
-        self.c0_traj.append(self.c0_position.copy())
-        self.c1_traj.append(self.c1_position.copy())
+        self.c0_traj = [self.c0_position.copy()]
+        self.c1_traj = [self.c1_position.copy()]
 
         return self.rod_pose 
         
@@ -73,16 +73,14 @@ class TwoCarrierEnv(gym.Env):
         done = False
         info = ''
         reward = 0
-        # prev_rod = self.rod_pose.copy()
+        prev_rod = self.rod_pose.copy()
         prev_c0 = self.c0_position.copy()
         prev_c1 = self.c1_position.copy()
         # compute rod's displacement and rotation
         disp = self.action_codebook[action[0]] + self.action_codebook[action[1]]
         rot = 0.
-        if disp[0]==0:
-            rot += -np.arctan2(self.action_codebook[action[0]][0]*np.sin(self.rod_pose[-1]), .5)
-        if disp[1]==0:
-            rot += np.arctan2(self.action_codebook[action[0]][1]*np.cos(self.rod_pose[-1]), .5)
+        rot += -np.arctan2(self.action_codebook[action[0]][0]*np.sin(self.rod_pose[-1]), .5) + \
+            np.arctan2(self.action_codebook[action[0]][1]*np.cos(self.rod_pose[-1]), .5)
         deltas = np.append(disp, rot)
         self.rod_pose += deltas
         self.c0_position = np.array([
@@ -110,22 +108,22 @@ class TwoCarrierEnv(gym.Env):
         # vertang = np.arccos(np.dot(uvec_vert, uvec_rod)) # angle between current rod and vertical vector
         # if vertang>np.pi/2:
         #     vertang = np.pi-vertang
-        # reward = 100*(np.abs(prev_rod[0])-np.abs(self.rod_pose[0])) + \
-        #     100*(self.rod_pose[1]-prev_rod[1]) + \
-        #     100*(prev_vertang-vertang) 
-        reward = 100*(np.abs(prev_c0[0])-np.abs(self.c0_position[0]) + np.abs(prev_c1[0])-np.abs(self.c1_position[0])) + \
-            100*(self.c0_position[1]-prev_c0[1] + self.c1_position[1]-prev_c1[1])
+        # reward = (np.abs(prev_rod[0])-np.abs(self.rod_pose[0])) + \
+        #     10*(self.rod_pose[1]-prev_rod[1]) + \
+        #     10*(prev_vertang-vertang) 
+        reward = np.abs(prev_c0[0])-np.abs(self.c0_position[0]) + np.abs(prev_c1[0])-np.abs(self.c1_position[0]) + \
+            (self.c0_position[1]-prev_c0[1] + self.c1_position[1]-prev_c1[1])
         
         # check crash
         rod_points = np.linspace(self.c0_position, self.c1_position, 50)
-        for p in self.wall_patches:
+        for p in self.fixed_patches:
             if np.sum(p.contains_points(rod_points, radius=.001)):
                 done = True
                 info = 'crash wall'
                 break
         # check escape
         if self.c0_position[1]>5.5 and self.c1_position[1]>5.5:
-            reward = 100
+            reward = 100.
             done = True
             info = 'escaped'
 
@@ -135,7 +133,7 @@ class TwoCarrierEnv(gym.Env):
         self.ax = self.fig.get_axes()[0]
         self.ax.cla()
         patch_list = []
-        patch_list += self.wall_patches
+        patch_list += self.fixed_patches
         # add wall patches
         c0pat = Circle(
             xy=(self.c0_position[0], self.c0_position[-1]), 
@@ -166,7 +164,7 @@ class TwoCarrierEnv(gym.Env):
             self.ax.plot(traj_c0[-100:,0], traj_c0[-100:,1], linestyle=':', linewidth=0.5, color='black')
             self.ax.plot(traj_c1[-100:,0], traj_c1[-100:,1], linestyle=':', linewidth=0.5, color='black')
         # Set ax
-        self.ax.axis(np.array([-5.6, 5.6, -.6, 6.6]))
+        self.ax.axis(np.array([-5.6, 5.6, -.6, 10.6]))
         plt.pause(0.02)
         self.fig.show()
 
@@ -174,10 +172,12 @@ class TwoCarrierEnv(gym.Env):
 # Uncomment following to test env
 # env = TwoCarrierEnv()
 # env.reset()
+# for _ in range(20):
+#     o,r,d,i = env.step([0,0])
 # for _ in range(env.max_episode_steps):
 #     env.render()
-#     o,r,d,i = env.step(np.random.randint(0,4,(2)))
-#     # o,r,d,i = env.step([0,1])
+#     # o,r,d,i = env.step(np.random.randint(0,4,(2)))
+#     o,r,d,i = env.step([1,2])
 #     print(o, r, d, i)
 #     if d:
 #         break