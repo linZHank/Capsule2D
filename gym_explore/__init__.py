3ce054d578bf5f3e83214185e4078cbd6df9f7cd
Lin Zhang <linzhank@gmail.com>
2022-06-23 15:11:16 -0500

    checkout new environment

--- gym_explore/__init__.py
+++ gym_explore/__init__.py
@@ -3,5 +3,6 @@ from gym.envs.registration import register
 register(
     id='escaper-v0',
     entry_point='gym_explore.envs:EscaperEnv',
+    max_episode_steps=1000,
 )
 
--- gym_explore/envs/escaper_env.py
+++ gym_explore/envs/escaper_env.py
@@ -16,28 +16,28 @@ class EscaperEnv(gym.Env):
         self.max_episode_steps = 1000
         self.continuous = continuous
         self.observation_space = spaces.Box(
-            low=-10.0, high=10.0, shape=(2,), dtype=np.float32
+            low=-10.0, high=10.0, shape=(3,), dtype=np.float32
         )
         if self.continuous:
             self.action_space = spaces.Box(
-                low=-0.4, high=0.4, shape=(2,), dtype=np.float32
+                low=np.array([-0.2, -np.pi/4]),
+                high=np.array([0.2, np.pi/4]),
+                shape=(2,),
+                dtype=np.float32,
             )
         else:
-            self.action_codebook = np.array(
-                [
-                    [0.0, 0.0],  # stop
-                    [0.0, 0.2],  # up
-                    [0.0, -0.2],  # down
-                    [-0.2, 0.0],  # left
-                    [0.2, 0.0],  # right
-                ]
-            )
-            self.action_space = spaces.Discrete(5)
+            self.action_space = spaces.Discrete(4)
+            self.action_codebook = {
+                0: np.array([0.1, np.pi / 8]),  # linear, angular
+                1: np.array([0.1, -np.pi / 8]),
+                2: np.array([-0.1, np.pi / 8]),
+                3: np.array([-0.1, -np.pi / 8]),
+            }
         # vars
-        self.position = np.zeros(2)
+        self.pose = np.zeros(3, dtype=np.float32)  # x, y, th
         self.trajectory = []
         # prepare renderer
-        self.fig = plt.figure(figsize=(7, 7))
+        self.fig = plt.figure(figsize=(8, 8))
         self.ax = self.fig.add_subplot(111)
         outer_wall = Circle(xy=(0, 0), radius=8.0, fc="grey", ec=None)
         inner_wall = Circle(xy=(0, 0), radius=7.0, fc="white", ec=None)
@@ -46,77 +46,76 @@ class EscaperEnv(gym.Env):
         )
         self.fixed_patches = [outer_wall, inner_wall, doorway]
 
-    # def seed(self, seed=None):
-    #     self.np_random, seed = seeding.np_random(seed)
-    #     return [seed]
-    #
     def reset(
         self,
-        *,
         seed: Optional[int] = None,
+        options: Optional[str] = None,
         return_info: bool = False,
     ):
         super().reset(seed=seed)
         self.step_counter = 0
-        r = np.random.uniform(low=-6.5, high=6.5)
-        th = np.random.uniform(low=-np.pi, high=np.pi)
-        x = r * np.cos(th)
-        y = r * np.sin(th)
-        self.position = np.array([x, y])  # escaper init position
-        self.trajectory = [self.position.copy()]
+        # reset escaper to origin or randomly
+        self.pose = np.zeros(3, dtype=np.float32)
+        if options == "random":
+            x = np.random.uniform(low=-6.5, high=6.5)
+            y = np.random.uniform(low=-6.5, high=6.5)
+            th = np.random.uniform(low=-np.pi, high=np.pi)
+            self.pose = np.array([x, y, th], dtype=np.float32)
+        self.trajectory = [self.pose[:2].copy()]
+        info = {"status": "reset"}
 
-        if not return_info:
-            return self.position
-        else:
-            return self.position, {}
+        return (self.pose, info) if return_info else self.pose
 
-    # TODO: upgrade to continuous action
     def step(self, action):
+        # compute displacement
         if self.continuous:
-            action = np.clip(action, -0.4, 0.4).astype(np.float32)
+            np.clip(
+                action, 
+                a_min=np.array([-0.2, -np.pi/4]),
+                a_max=np.array([0.2, np.pi/4]),
+                out=action,
+            )
+            vx = action[0]
+            vth = action[1]
         else:
             assert self.action_space.contains(
                 action
             ), f"{action!r} ({type(action)}) invalid "
+            vx = self.action_codebook[action][0]
+            vth = self.action_codebook[action][1]
         done = False
         info = {"status": "trapped"}
         reward = 0
-        # compute displacement
-        prev_pos = self.position.copy()
-        if self.continuous:
-            self.position += action
-        else:
-            self.position += self.action_codebook[action]
+        # compute new position
+        prev_pose = self.pose.copy()
+        dx = vx * np.cos(prev_pose[-1])
+        dy = vx * np.sin(prev_pose[-1])
+        dth = vth
+        self.pose += np.array([dx, dy, dth])
+        if self.pose[-1] > np.pi:  # orientation within [-pi, pi]
+            self.pose[-1] -= 2*np.pi
+        elif self.pose[-1] < -np.pi:
+            self.pose[-1] += 2*np.pi
         # compute reward
         reward = (
-            np.abs(prev_pos[0])
-            - np.abs(self.position[0])
-            + self.position[1]
-            - prev_pos[1]
-        )
-        self.trajectory.append(self.position.copy())
+            np.abs(prev_pose[0])
+            - np.abs(self.pose[0])
+            + self.pose[1]
+            - prev_pose[1]
+        )  # |x0 - x1| + (y1 - y0)
+        self.trajectory.append(self.pose[:2].copy())
         # check crash
-        if self.fixed_patches[0].contains_point(self.position, radius=0.1):
-            if not self.fixed_patches[1].contains_point(self.position, radius=0.1):
-                if not self.fixed_patches[2].contains_point(self.position, radius=0.1):
+        if self.fixed_patches[0].contains_point(self.pose[:2], radius=0.1):
+            if not self.fixed_patches[1].contains_point(self.pose[:2], radius=0.1):
+                if not self.fixed_patches[2].contains_point(self.pose[:2], radius=0.1):
                     done = True
-                    info = {"status": "crash wall"}
+                    info = {"status": "crash"}
         else:
             reward = 100.0
             done = True
             info = {"status": "escaped"}
-        # for pat in self.fixed_patches:
-        #     if np.sum(pat.contains_point(self.position, radius=0.001)):
-        #         done = True
-        #         info = {"status": "crash wall"}
-        #         break
-        # check escape
-        # if self.position[1] > 5.5:
-        #     reward = 100.0
-        #     done = True
-        #     info = {"status": "escaped"}
 
-        return self.position, reward, done, info
+        return self.pose, reward, done, info
 
     def render(self, mode="human"):
         self.ax = self.fig.get_axes()[0]
@@ -125,7 +124,8 @@ class EscaperEnv(gym.Env):
         patch_list += self.fixed_patches
         # add wall patches
         escaper_pat = Circle(
-            xy=(self.position[0], self.position[-1]),
+            # xy=(self.position[0], self.position[-1]),
+            xy=(self.pose[0], self.pose[1]),
             radius=0.2,
             ec="blueviolet",
             fc="white",
@@ -153,6 +153,9 @@ class EscaperEnv(gym.Env):
         self.ax.grid(color="grey", linestyle=":", linewidth=0.5)
         plt.pause(0.02)
         self.fig.show()
+
+    def close(self):
+        plt.close("all")
 
 
 # Uncomment following to test env
