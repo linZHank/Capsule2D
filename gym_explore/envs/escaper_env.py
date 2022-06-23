fbde5f3c9fb87204b387e8e790a2642a1cedbb19
Lin Zhang <linzhank@gmail.com>
2022-06-23 11:54:15 -0500

    update escapers action

--- gym_explore/envs/escaper_env.py
+++ gym_explore/envs/escaper_env.py
@@ -16,20 +16,20 @@ class EscaperEnv(gym.Env):
         self.max_episode_steps = 1000
         self.continuous = continuous
         self.observation_space = spaces.Box(
-            low=-10.0, high=10.0, shape=(2,), dtype=np.float32
+            low=-10.0, high=10.0, shape=(3,), dtype=np.float32
         )
         if self.continuous:
+            # self.action_space = spaces.Box(
+            #     low=-0.4, high=0.4, shape=(2,), dtype=np.float32
+            # )
             self.action_space = spaces.Box(
-                low=-0.4, high=0.4, shape=(2,), dtype=np.float32
+                low=np.array([-0.4, -np.pi/3]),
+                high=np.array([0.4, np.pi/3]),
+                shape=(2,), 
+                dtype=np.float32,
             )
         else:
             self.action_space = spaces.Discrete(4)
-            self.action_codebook = {
-                0: np.array([0.0, 0.2]),  # up
-                1: np.array([0.0, -0.2]),  # down
-                2: np.array([-0.2, 0.0]),  # left
-                3: np.array([0.2, 0]),  # right
-            }
             # self.action_codebook = np.array(
             #     [
             #         [0.0, 0.2],  # up
@@ -38,8 +38,15 @@ class EscaperEnv(gym.Env):
             #         [0.2, 0.0],  # right
             #     ]
             # )
+            self.action_codebook = {
+                0: np.array([0.2, np.pi / 6]),  # forward left
+                1: np.array([0.2, -np.pi / 6]),  # forward right
+                2: np.array([-0.2, np.pi / 6]),  # backward right
+                3: np.array([-0.2, -np.pi / 6]),  # backward left
+            }
         # vars
-        self.position = np.zeros(2)
+        self.pose = np.zeros(3, dtype=np.float32)  # x, y, th
+        # self.position = np.zeros(2)  # x, y
         self.trajectory = []
         # prepare renderer
         self.fig = plt.figure(figsize=(8, 8))
@@ -64,47 +71,68 @@ class EscaperEnv(gym.Env):
         super().reset(seed=seed)
         self.step_counter = 0
         # reset escaper to origin or randomly
-        r = 0.
-        th = 0.
+        # r = 0.0
+        # th = 0.0
+        self.pose = np.zeros(3, dtype=np.float32)
         if options == "random":
-            r = np.random.uniform(low=-6.5, high=6.5)
+            # r = np.random.uniform(low=-6.5, high=6.5)
+            # th = np.random.uniform(low=-np.pi, high=np.pi)
+            x = np.random.uniform(low=-6.5, high=6.5)
+            y = np.random.uniform(low=-6.5, high=6.5)
             th = np.random.uniform(low=-np.pi, high=np.pi)
-        x = r * np.cos(th)
-        y = r * np.sin(th)
-        self.position = np.array([x, y], dtype=np.float32)  # escaper init position
-        print(f'obs: {self.position}')
-        self.trajectory = [self.position.copy()]
+            self.pose = np.array([x, y, th], dtype=np.float32)
+        # x = r * np.cos(th)
+        # y = r * np.sin(th)
+        # self.position = np.array([x, y], dtype=np.float32)  # init position
+        # self.trajectory = [self.position.copy()]
+        self.trajectory = [self.pose[:2].copy()]
         info = {"status": "reset"}
 
-        return (self.position, info) if return_info else self.position
+        # return (self.position, info) if return_info else self.position
+        return (self.pose, info) if return_info else self.pose
 
     def step(self, action):
         # compute displacement
         if self.continuous:
-            ds = np.clip(action, -0.4, 0.4).astype(np.float32)
+            # ds = np.clip(action, -0.4, 0.4).astype(np.float32)
+            vx = action[0]
+            vth = action[1]
         else:
             assert self.action_space.contains(
                 action
             ), f"{action!r} ({type(action)}) invalid "
-            ds = self.action_codebook[action]
+            # ds = self.action_codebook[action]
+            vx = self.action_codebook[action][0]
+            vth = self.action_codebook[action][1]
         done = False
         info = {"status": "trapped"}
         reward = 0
         # compute new position
-        prev_pos = self.position.copy()
-        self.position += ds
+        prev_pose = self.pose.copy()
+        dx = vx * np.cos(prev_pose[-1])
+        dy = vx * np.sin(prev_pose[-1])
+        dth = vth
+        # self.position += ds
+        self.pose += np.array([dx, dy, dth])
         # compute reward
+        # reward = (
+        #     np.abs(prev_pos[0])
+        #     - np.abs(self.position[0])
+        #     + self.position[1]
+        #     - prev_pos[1]
+        # )
         reward = (
-            np.abs(prev_pos[0])
+            np.abs(prev_pose[0])
             - np.abs(self.position[0])
             + self.position[1]
-            - prev_pos[1]
-        )
-        self.trajectory.append(self.position.copy())
+            - prev_pose[1]
+        )  # |x0 - x1| + (y1 - y0)
+        # self.trajectory.append(self.position.copy())
+        self.trajectory.append(self.pose[:2].copy())
         # check crash
-        if self.fixed_patches[0].contains_point(self.position, radius=0.1):
-            if not self.fixed_patches[1].contains_point(self.position, radius=0.1):
-                if not self.fixed_patches[2].contains_point(self.position, radius=0.1):
+        if self.fixed_patches[0].contains_point(self.pose[:2], radius=0.1):
+            if not self.fixed_patches[1].contains_point(self.pose[:2], radius=0.1):
+                if not self.fixed_patches[2].contains_point(self.pose[:2], radius=0.1):
                     done = True
                     info = {"status": "crash"}
         else:
@@ -131,7 +159,8 @@ class EscaperEnv(gym.Env):
         patch_list += self.fixed_patches
         # add wall patches
         escaper_pat = Circle(
-            xy=(self.position[0], self.position[-1]),
+            # xy=(self.position[0], self.position[-1]),
+            xy=(self.pose[0], self.pose[1]),
             radius=0.2,
             ec="blueviolet",
             fc="white",
