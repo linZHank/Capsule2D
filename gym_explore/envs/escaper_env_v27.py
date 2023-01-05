393d1d528ebcac2ae333df0f3f58fdd646f340c6
Lin Zhang <linzhank@gmail.com>
2023-01-05 07:42:42 -0600

    update step()

--- gym_explore/envs/escaper_env_v27.py
+++ gym_explore/envs/escaper_env_v27.py
@@ -7,7 +7,7 @@ import gymnasium as gym
 from gymnasium import spaces
 
 
-# TODO:
+# TODO: steps limit and truncated condition
 class EscaperEnv(gym.Env):
     metadata = {"render_modes": ["human"], "render_fps": 50}
 
@@ -34,12 +34,14 @@ class EscaperEnv(gym.Env):
                 3: np.array([-0.1, -np.pi / 8]),
             }
         # vars
-        self._agent_pose = np.zeros(3, dtype=np.float32)  # x, y, th
+        self._agent_pose = None
         self._agent_traj = []
         self._agent_status = None
         # render mode
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
+        self._fig = None
+        self._ax = None
 
     def _get_obs(self):
         return self._agent_pose
@@ -56,7 +58,7 @@ class EscaperEnv(gym.Env):
         super().reset(seed=seed)
         self.step_counter = 0
         # reset escaper to origin or randomly
-        self._agent_pose = np.zeros(3, dtype=np.float32)
+        self._agent_pose = np.zeros(3, dtype=np.float32)  # x, y, th
         if options == "random":
             x = np.random.uniform(low=-6.5, high=6.5)
             y = np.random.uniform(low=-6.5, high=6.5)
@@ -64,25 +66,83 @@ class EscaperEnv(gym.Env):
             self._agent_pose = np.array([x, y, th], dtype=np.float32)
         self._agent_traj.append(self._agent_pose[:2].copy())
         # get obs and info
+        self._agent_status = "trapped"
         observation = self._get_obs()
         info = self._get_info()
         # prepare renderer
         if self.render_mode == "human":
             self._fig = plt.figure(figsize=(8, 8))
             self._ax = self._fig.add_subplot(111)
+            # init fixed patches
             outer_wall = Circle(xy=(0, 0), radius=8.0, fc="grey", ec=None)
             inner_wall = Circle(xy=(0, 0), radius=7.0, fc="white", ec=None)
             doorway = Wedge(
                 center=(0, 0), r=8.0, theta1=85.0, theta2=95, fc="white", ec=None
             )
             self._fixed_patches = [outer_wall, inner_wall, doorway]
+            # render
             self._render_frame()
 
         return observation, info
 
-    # def render(self):
-    #     if self.render_mode == "rgb_array":
-    #         return self._render_frame()
+    def step(self, action):
+        # compute displacement
+        if self.continuous:
+            np.clip(
+                action,
+                a_min=np.array([-0.2, -np.pi/4]),
+                a_max=np.array([0.2, np.pi/4]),
+                out=action,
+            )
+            vx = action[0]
+            vth = action[1]
+        else:
+            assert self.action_space.contains(
+                action
+            ), f"{action!r} ({type(action)}) invalid "
+            vx = self.action_codebook[action][0]
+            vth = self.action_codebook[action][1]
+        # init returns
+        observation = None
+        terminated = False
+        truncated = False
+        reward = None
+        info = None
+        # compute new position
+        prev_pose = self._agent_pose.copy()
+        dx = vx * np.cos(prev_pose[-1])
+        dy = vx * np.sin(prev_pose[-1])
+        dth = vth
+        self._agent_pose += np.array([dx, dy, dth])
+        if self._agent_pose[-1] > np.pi:  # orientation within [-pi, pi]
+            self._agent_pose[-1] -= 2*np.pi
+        elif self._agent_pose[-1] < -np.pi:
+            self._agent_pose[-1] += 2*np.pi
+        observation = self._get_obs()
+        # compute reward
+        reward = (
+            np.abs(prev_pose[0])
+            - np.abs(self._agent_pose[0])
+            + self._agent_pose[1]
+            - prev_pose[1]
+        )  # |x0 - x1| + (y1 - y0)
+        self._agent_traj.append(self._agent_pose[:2].copy())
+        # check crash
+        if self._fixed_patches[0].contains_point(self._agent_pose[:2], radius=0.1):
+            if not self._fixed_patches[1].contains_point(self._agent_pose[:2], radius=0.1):
+                if not self._fixed_patches[2].contains_point(self._agent_pose[:2], radius=0.1):
+                    terminated = True
+                    self._agent_status = "crash"
+        else:
+            reward = 100.0
+            terminated = True
+            self._agent_status = "escaped"
+        info = self._get_info()
+        # render
+        if self.render_mode == "human":
+            self._render_frame()
+
+        return observation, reward, terminated, truncated, info
 
     def _render_frame(self):
         self._ax = self._fig.get_axes()[0]
@@ -118,72 +178,26 @@ class EscaperEnv(gym.Env):
         self._ax.set_xticks(np.arange(-10, 10))
         self._ax.set_yticks(np.arange(-10, 10))
         self._ax.grid(color="grey", linestyle=":", linewidth=0.5)
-        plt.pause(0.02)
+        # update figure
+        plt.pause(1 / self.metadata["render_fps"])
         self._fig.show()
 
-    # def step(self, action):
-    #     # compute displacement
-    #     if self.continuous:
-    #         np.clip(
-    #             action,
-    #             a_min=np.array([-0.2, -np.pi/4]),
-    #             a_max=np.array([0.2, np.pi/4]),
-    #             out=action,
-    #         )
-    #         vx = action[0]
-    #         vth = action[1]
-    #     else:
-    #         assert self.action_space.contains(
-    #             action
-    #         ), f"{action!r} ({type(action)}) invalid "
-    #         vx = self.action_codebook[action][0]
-    #         vth = self.action_codebook[action][1]
-    #     done = False
-    #     info = {"status": "trapped"}
-    #     reward = 0
-    #     # compute new position
-    #     prev_pose = self.pose.copy()
-    #     dx = vx * np.cos(prev_pose[-1])
-    #     dy = vx * np.sin(prev_pose[-1])
-    #     dth = vth
-    #     self.pose += np.array([dx, dy, dth])
-    #     if self.pose[-1] > np.pi:  # orientation within [-pi, pi]
-    #         self.pose[-1] -= 2*np.pi
-    #     elif self.pose[-1] < -np.pi:
-    #         self.pose[-1] += 2*np.pi
-    #     # compute reward
-    #     reward = (
-    #         np.abs(prev_pose[0])
-    #         - np.abs(self.pose[0])
-    #         + self.pose[1]
-    #         - prev_pose[1]
-    #     )  # |x0 - x1| + (y1 - y0)
-    #     self.trajectory.append(self.pose[:2].copy())
-    #     # check crash
-    #     if self.fixed_patches[0].contains_point(self.pose[:2], radius=0.1):
-    #         if not self.fixed_patches[1].contains_point(self.pose[:2], radius=0.1):
-    #             if not self.fixed_patches[2].contains_point(self.pose[:2], radius=0.1):
-    #                 done = True
-    #                 info = {"status": "crash"}
-    #     else:
-    #         reward = 100.0
-    #         done = True
-    #         info = {"status": "escaped"}
-    #
-    #     return self.pose, reward, done, info
-    #
-    #
-    # def close(self):
-    #     plt.close("all")
+    # TODO: for future usage
+    # def render(self):
+    #     if self.render_mode == "rgb_array":
+    #         return self._render_frame()
+
+    def close(self):
+        plt.close("all")
 
 
 # Uncomment following to test env
 env = EscaperEnv(render_mode="human", continuous=False)
 obs, info = env.reset()
-# for _ in range(1000):
-#     env.render()
-#     o, r, d, i = env.step(env.action_space.sample())
-#     # o,r,d,i = env.step([1,2])
-#     print(o, r, d, i)
-#     if d:
-#         break
+for _ in range(1000):
+    o, r, d, t, i = env.step(env.action_space.sample())
+    # o,r,d,i = env.step([1,2])
+    print(o, r, d, t, i)
+    if d:
+        env.reset()
+env.close()
