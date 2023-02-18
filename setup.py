287149f2c1bff6160deeac22862d054d74efbba8
Lin Zhang <linzhank@gmail.com>
2023-02-18 08:17:04 -0600

    minor update env
    import escaper_env or make such a env will no longer execute the testing
    snippet.

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='gym_explore',
     version='0.0.1',
     install_requires=[
-        'gymnasium>=0.27.0',
+        'gymnasium==0.27.0',
         'numpy>=1.23',
-        'matplotlib>=3.6.2',
+        'matplotlib>=3.6.0',
     ]
 )
