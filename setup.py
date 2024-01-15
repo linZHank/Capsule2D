70511ca859dac7cae4bd02f7dd96bd304f9f45fb
Lin Zhang <linzhank@gmail.com>
2024-01-15 09:30:37 -0600

    update gym version

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='gym_explore',
     version='0.0.1',
     install_requires=[
-        'gymnasium==0.27.0',
+        'gymnasium>=0.28.1',
         'numpy>=1.23',
-        'matplotlib>=3.6.0',
+        'matplotlib>=3.7.2',
     ]
 )
